c025f7160d9dee5b0642de24a41fa154c8566f78
Christopher Kennedy <chris94kennedy@gmail.com>
2019-07-23 12:06:18 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -9,6 +9,6 @@ def introduction_with_language(name, language)
 end
 #test 2 passed
 
-def introduction_with_language_optional(name, language)
+def introduction_with_language_optional(name, language = "Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
